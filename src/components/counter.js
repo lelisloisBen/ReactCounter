e7c84378d8e3995413396c1db54183d77cde8718
samir benzada <samirbenzada@gmail.com>
2019-10-08 17:01:35 -0400

    sick numbers

---
+++ src/components/counter.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+const Counter = () => {
+
+    const [first, setFirst] = useState(0);
+    const [second, setSecond] = useState(0);
+    // const [third, setThird] = useState(0);
+    // const [fourth, setFourth] = useState(0);
+    // const [fifth, setFifth] = useState(0);
+    // const [sixth, setSixth] = useState(0);
+
+    let timer = (varr, setter) => {
+        setTimeout(() => {
+            setter(varr + 1);
+        },1000)   
+    }
+
+
+useEffect(() => {
+    if (first === 10) {
+        setFirst(0);
+        timer(first, setFirst)
+    } else {
+        timer(first, setFirst)
+    }
+},[first, setFirst]);
+
+useEffect(() => {
+    setTimeout(() => {
+        if (second === 10) {
+            setSecond(0);
+            timer(second, setSecond)
+        } else {
+            timer(second, setSecond)
+        }
+    },9000)
+},[second, setSecond]);
+
+    
+
+    
+
+
+    return (
+        <>
+            <div className="mainCountDiv container text-center">
+                <div className="num_tiles">
+                    {second}
+                </div>
+                <div className="num_tiles">
+                    {first}
+                </div>
+                
+            </div>
+        </>
+    );
+};
+
+export default Counter;
