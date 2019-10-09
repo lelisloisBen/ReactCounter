ccb4a6f3bd554f1778dbb56b6f5841f842b433ac
samir benzada <samirbenzada@gmail.com>
2019-10-08 21:09:56 -0400

    Counter Done

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
-
-
+import Footer from './footer';
 
 const Counter = () => {
 
     const [first, setFirst] = useState(0);
     const [second, setSecond] = useState(0);
-    // const [third, setThird] = useState(0);
-    // const [fourth, setFourth] = useState(0);
-    // const [fifth, setFifth] = useState(0);
-    // const [sixth, setSixth] = useState(0);
+    const [third, setThird] = useState(0);
+    const [fourth, setFourth] = useState(0);
+    const [fifth, setFifth] = useState(0);
+    const [sixth, setSixth] = useState(0);
 
     let timer = (varr, setter) => {
         setTimeout(() => {
@@ -17,32 +16,45 @@ const Counter = () => {
         },1000)   
     }
 
+    let addOne = (varr, setter) => {
+        setter(varr + 1);
+    }
 
     useEffect(() => {
-        if (first === 9) timer(second, setSecond);
-        if (first === 10) {
-            setFirst(0);
-        } else {
-            timer(first, setFirst);
-        }
-    },[first]);
-
-    
-
-    
-
+        if (first === 10) { setFirst(0); addOne(second, setSecond); } else { timer(first, setFirst) }
+        if (second === 6) { setSecond(0); addOne(third, setThird); };
+        if (third === 10) { setThird(0); addOne(fourth, setFourth); };
+        if (fifth === 6) { setFifth(0); addOne(sixth, setSixth); };
+        if (sixth === 2 && fifth === 4) { setSixth(0); setFifth(0); setThird(0); setSecond(0); setFirst(0);}
+    }, [first, second, third, fourth, fifth, sixth]);
 
     return (
-        <>
+        <>  
+            <div className="container text-center mt-5">
+                <div class="jumbotron"><h1>REACT COUNTER</h1></div>
+            </div>
+            
             <div className="mainCountDiv container text-center">
+                <div className="num_tiles">
+                    {sixth}
+                </div>
+                <div className="num_tiles">
+                    {fifth}
+                </div>
+                <div className="num_tiles">
+                    {fourth}
+                </div>
+                <div className="num_tiles">
+                    {third}
+                </div>
                 <div className="num_tiles">
                     {second}
                 </div>
                 <div className="num_tiles">
                     {first}
                 </div>
-                
             </div>
+            <Footer/>
         </>
     );
 };
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="footer mt-auto py-3 fixed-bottom">
+            <div class="container text-center">
+                <span class="text-muted">Made By Samir Benzada, 4GeeksAcademy Student.</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
